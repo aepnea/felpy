a987fbd4475ee5e61118c348debe5bfbcbeb505a
kana <cvasquez@zaetachile.cl>
2015-01-13 11:12:26 -0300

    se agrega al seed, las regiones

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,4 @@
 class Region < ActiveRecord::Base
   has_many :comunas
+  has_many :orders
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+#poblando regiones
+Region.create([{name: 'Región de Arica y Parinacota', number: 'XV'}, \
+         {name: 'Región de Tarapacá', number: 'I'}, \
+         {name: 'Región de Antofagasta', number: 'II'}, \
+         {name: 'Región de Atacama', number: 'III'}, \
+         {name: 'Región de Coquimbo', number: 'IV'}, \
+         {name: 'Región de Valparaíso', number: 'V'}, \
+         {name: 'Región Metropolitana', number: 'XIII'}, \
+         {name: 'Región del Libertador General Bernardo O\'Higgins', number: 'VI'}, \
+         {name: 'Región del Maule', number: 'VII'}, \
+         {name: 'Región del Biobío', number: 'VIII'}, \
+         {name: 'Región de La Araucanía', number: 'IX'}, \
+         {name: 'Región de Los Ríos', number: 'XIV'}, \
+         {name: 'Región de Los Lagos', number: 'X'}, \
+         {name: 'Región de Aysén', number: 'XI'}, \
+         {name: 'Región de Magallanes', number: 'XII'}])
